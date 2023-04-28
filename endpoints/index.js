04f5d108c6fd3fa75aee8077762136a7f72aef98
Phakphum Kunakong <icedev001@gmail.com>
2023-04-28 18:50:12 +0700

    remove simple gdrive

--- endpoints/index.js
+++ endpoints/index.js
@@ -5,43 +5,7 @@ module.exports = function (app) {
 	
     app.get('/', async (req, res) => {
         // #swagger.ignore = true
-        try{
-            const { googleDriveV3 } = await googleDrive('ssGerCn6y69GgzFhyXfmjA==');
-            const result = await googleDriveV3.files.list({
-                pageSize: 10,
-                fields: "nextPageToken, files(id, name)",
-            });
-            res.status(200).send(result.data);
-        } catch (err) {
-            res
-            .status(500)
-            .send({ type: "Error", message: err.message, stack: err.stack });
-        }
-    });
-
-    app.get('/testfile', async (req, res) => {
-        // #swagger.ignore = true
-        try{
-            const { googleDriveV3 } = await googleDrive('ssGerCn6y69GgzFhyXfmjA==');
-
-            const fileMimeType = "text/plain";
-            const result = await googleDriveV3.files.create({
-                requestBody: {
-                  name: "testfile-from-nodejs.txt",
-                  mimeType: fileMimeType,
-                  parents: ["1dY1s1gMMHShjlsmiqA6DnzWjRK7DZQpc"],
-                },
-                media: {
-                  mimeType: fileMimeType,
-                  body: "test data",
-                },
-            });
-            res.status(200).send(result.data);
-        } catch (err) {
-            res
-            .status(500)
-            .send({ type: "Error", message: err.message, stack: err.stack });
-        }
+        return res.status(200).send(`Start API Keeper`);
     });
 
     app.get('/download', (req, res) => {
